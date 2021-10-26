7c5d0b4ba924ca8a1a2f2d78043af920013d50d6
Eimantas <eim.kasp@gmail.com>
2021-10-26 18:54:49 +0300

    Sukureme character service su Demo duomenimis

---
+++ src/app/character.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+
+
+/* Angular Dekoratorius */
+@Injectable({
+  providedIn: 'root'
+})
+
+/* Service tipo klases atsakingos uz darba su duomenimis */
+export class CharacterService {
+
+  //
+  constructor() { }
+
+  getCharacters() {
+      return [
+        {
+          "id": 1,
+          "name": "Rick Sanchez",
+          "status": "Alive",
+          "species": "Human",
+          "type": "",
+          "gender": "Male",
+          "origin": {
+            "name": "Earth",
+            "url": "https://rickandmortyapi.com/api/location/1"
+          },
+          "location": {
+            "name": "Earth",
+            "url": "https://rickandmortyapi.com/api/location/20"
+          },
+          "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+          "episode": [
+            "https://rickandmortyapi.com/api/episode/1",
+            "https://rickandmortyapi.com/api/episode/2",
+            // ...
+          ],
+          "url": "https://rickandmortyapi.com/api/character/1",
+          "created": "2017-11-04T18:48:46.250Z"
+        },
+        {
+          "id": 1,
+          "name": "Morty Smith",
+          "status": "Alive",
+          "species": "Human",
+          "type": "",
+          "gender": "Male",
+          "origin": {
+            "name": "Earth",
+            "url": "https://rickandmortyapi.com/api/location/1"
+          },
+          "location": {
+            "name": "Earth",
+            "url": "https://rickandmortyapi.com/api/location/20"
+          },
+          "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+          "episode": [
+            "https://rickandmortyapi.com/api/episode/1",
+            "https://rickandmortyapi.com/api/episode/2",
+          ],
+          "url": "https://rickandmortyapi.com/api/character/1",
+          "created": "2017-11-04T18:48:46.250Z"
+        }
+      ];
+  }
+
+}
