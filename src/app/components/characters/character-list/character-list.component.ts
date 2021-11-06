397cb906a4b2febf979e171b66bc0017ba13e6d5
Eimantas <eim.kasp@gmail.com>
2021-11-06 10:32:54 +0200

    Search 404 error handling

--- src/app/components/characters/character-list/character-list.component.ts
+++ src/app/components/characters/character-list/character-list.component.ts
@@ -12,11 +12,14 @@ export class CharacterListComponent implements OnInit {
   public characters: any = [];
   public charactersInfo: any = {};
 
+
+  public errors: any;
+
   public page: number = 1;
 
   public searchOptions = {
-    'name' : '',
-    'status' : ''
+    'name': '',
+    'status': ''
   }
 
   // "Injectiname" character service i komponenta
@@ -25,7 +28,7 @@ export class CharacterListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-      this.getCharacters();
+    this.getCharacters();
   }
 
   filterCharacters(name: string) {
@@ -34,29 +37,56 @@ export class CharacterListComponent implements OnInit {
     this.getCharacters();
   }
 
-  getCharacters(name ? : string) {
+  getCharacters(name?: string) {
     this._characterService.getCharacters(this.page, this.searchOptions.name)
-    // Subscribe funkcija naudojama dirbant su Observable tipo objektais (Angular httpClient visada grazina Observabile tipa)
-    // data - kintamasis su grazintais duomenimis is musu uzklausos
-    .subscribe((data: any) => {
-      // Gautus duomenis priskiriame komponento kintamajam
-      // Characters kintamajam, priskiriame duomenis is characterService getCharaters funkcijos
+      // Subscribe funkcija naudojama dirbant su Observable tipo objektais (Angular httpClient visada grazina Observabile tipa)
+      // data - kintamasis su grazintais duomenimis is musu uzklausos
+      .subscribe(
+        // Jei uzklausa buvo ivykdyta sekmingai
+        // Success callback
+        (data: any) => {
+          // Gautus duomenis priskiriame komponento kintamajam
+          // Characters kintamajam, priskiriame duomenis is characterService getCharaters funkcijos
+          this.characters = data.results;
+          this.charactersInfo = data.info;
+        },
 
-      this.characters = data.results;
-      this.charactersInfo = data.info;
-      // console.log(data);
-      /*
-      Dokumentacija kokie duomenys grazinami:
-      https://rickandmortyapi.com/documentation/#character-schema
-      */
+        // Jei grazintas error status code
+        // Error callback
+        (error: any) => {
 
-    });
+          // Patikriname error status koda
+          if (error.status == '404') {
+            alert("oops nothing found");
+            console.log(error);
+
+            // Priskiriame error objekta savo komponento errors masyvui,
+            // kad galetume atvaizduoti klaidos pranesima template dalyje
+            this.errors = error;
+
+            // Nustatome characters masyva i tuscia masyva, nes nebuvo rasta jokiu veikeju
+            this.characters = [];
+            // Taip pat pakeiciame charactersInfo objekto reiksmes pagal klaidos koda
+            this.charactersInfo.count = 0;
+            this.charactersInfo.pages = 0;
+          } else {
+            alert("oops went wrong");
+          }
+
+        }
+
+        // console.log(data);
+        /*
+        Dokumentacija kokie duomenys grazinami:
+        https://rickandmortyapi.com/documentation/#character-schema
+        */
+      );
   }
 
   nextPage() {
 
     // Pridedame vieneta jei puslapio skaicius yra mazesnis uz bendra puslapiu kieki
-    if(this.page < this.charactersInfo.pages) {
+    if (this.page < this.charactersInfo.pages) {
       this.page++;
     } else {
       alert("This is a last page");
@@ -71,7 +101,7 @@ export class CharacterListComponent implements OnInit {
   previousPage() {
 
     // Patikriname ar page reiksme nera neigiama, -1 puslapio nera
-    if(this.page > 1) {
+    if (this.page > 1) {
       this.page--;
     }
 
